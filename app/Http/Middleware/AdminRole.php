6534479eb5405fbe4b24420d6385bac8e53fc7e7
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-06-23 13:10:30 +0545

    admin middleware added

---
+++ app/Http/Middleware/AdminRole.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Constants\Constants;
+use App\Traits\ResponseTrait;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminRole
+{
+    use ResponseTrait;
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! auth()->user() || ! auth()->user()->hasRole(Constants::ROLE_ADMIN)) {
+            return $this->apiError('Forbidden', 401);
+            // return response()->json(['error' => 'Forbidden'], 401);
+        }
+
+        return $next($request);
+    }
+}
