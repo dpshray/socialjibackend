05058fe086d2fdbc77a596b1e3312d41eb1274d7
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-06-23 13:09:49 +0545

    currency migration and model file

---
+++ database/migrations/2025_06_23_115659_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->string('symbol');
+            $table->softDeletes();
+        });
+        DB::table('currencies')->insert([
+            ['name' => 'nepali rupee', 'code' => 'NPR', 'symbol' => 'रु'],
+            ['name' => 'us dollar', 'code' => 'USD', 'symbol' => '$']
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
