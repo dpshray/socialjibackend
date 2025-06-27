f3fa3b952dda90f5f7169eccc4a047971aa3f98a
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-06-27 12:06:19 +0545

    removed tag name uniqueness

--- database/migrations/2025_02_26_052302_create_tags_table.php
+++ database/migrations/2025_02_26_052302_create_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->integer('id')->autoIncrement()->primary();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->integer('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             // $table->foreignId('user_id')->constrained('users');
