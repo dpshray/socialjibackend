9e5ebc1fa77005dfb2e8016bc50eac3ef702d451
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-06-30 12:54:18 +0545

    added influencer about(column) w resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -26,8 +26,9 @@ class UserResource extends JsonResource
             "email" => $this->when($this->email, $this->email),
             "roles" => $this->whenLoaded('roles', fn() => $this->getRoleNames()->first()),
             'image' => $this->whenLoaded('media', fn() => $this->getFirstMediaUrl(Constants::MEDIA_USER)),
-            "influencer_rating" => $this->whenLoaded('influencerRatings', function(){
-                $rating = $this->influencerRatings->avg('rating');
+            'about' => $this->about,
+            "influencer_rating" => $this->whenLoaded('brandReviews', function(){
+                $rating = $this->brandReviews->avg('rating');
                 return ($rating <=0) ? 0 : round($rating,1);
             }),
             'social_profiles' => $this->whenLoaded('socialProfiles', function(){
@@ -37,15 +38,3 @@ class UserResource extends JsonResource
         ];
     }
 }
-
-
-// "social_site_id": 1,
-// "profile_url": "https://gutmann.com/cumque-adipisci-sint-excepturi.html",
-// "follower_count": 23069,
-// "following_count": 21427,
-// "post_count": 41968,
-// "avg_like_per_post_count": 35,
-// "avg_comment_per_post_count": 30,
-// "follower_growth_rate_per_week": 47,
-// "highest_like": 835008,
-// "lowest_like": 1577
