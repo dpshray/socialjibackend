f69f0934a97df89be7a40ffeb5aa932645f9e798
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-07-03 17:39:04 +0545

    cors changes

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,28 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    // 'paths' => ['*'],
 
+    // 'allowed_methods' => ['*'],
+
+    // 'allowed_origins' => ['*'],
+
+    // 'allowed_origins_patterns' => [],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => true,
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => ['https://demo.stage.dworklabs.com'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
 
 ];
