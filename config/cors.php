35f925a5111b1df6dcbe025efedea2d8811827bc
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-07-04 10:55:11 +0545

    cors changed(for testing)

--- config/cors.php
+++ config/cors.php
@@ -14,29 +14,20 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
-
     // 'paths' => ['*'],
-
     // 'allowed_methods' => ['*'],
-
     // 'allowed_origins' => ['*'],
-
     // 'allowed_origins_patterns' => [],
-
     // 'allowed_headers' => ['*'],
-
     // 'exposed_headers' => [],
-
     // 'max_age' => 0,
-
     // 'supports_credentials' => true,
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://demo.stage.dworklabs.com'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-
 ];
