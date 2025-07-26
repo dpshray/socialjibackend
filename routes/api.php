bec5f51278197ab344a72f4fe881577a77bfc9e5
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-07-27 03:00:46 +0545

    moved log url to api.php

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Middleware\InfluencerRole;
 use App\Http\Middleware\JwtMiddleware;
 use App\Http\Middleware\VerifyEmail;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 require __DIR__.'/auth.php';
 require __DIR__.'/payment.php';
@@ -49,5 +50,27 @@ Route::middleware([JwtMiddleware::class, VerifyEmail::class])->group(function ()
     Route::middleware([AdminRole::class])->prefix('admin')->group(function(){
         Route::apiResource('currency', CurrencyController::class);
     });
+});
+
+Route::get('all-log-files', function () {
+    $files = File::files(storage_path('logs'));
+    echo "<ul>";
+    foreach ($files as $file) {
+        echo "<li>" . $file->getFilename() . "</li>"; // Show log file name like laravel-2025-07-24.log
+    }
+    echo "</ul>";
+});
+
+Route::get('view-log-files/{date}', function ($date = null) {
+    if (empty($date)) {
+        return 'date is required';
+    }
+    $date = $date ?? now()->format('Y-m-d'); // Default to today
+    $logPath = storage_path("logs/laravel-{$date}.log");
+    if (!File::exists($logPath)) {
+        abort(404, 'Log file not found');
+    }
+    $logs = File::get($logPath); // Get the whole log content
+    return response()->view('logs-view', ['logs' => $logs]);
 });
 // Route::get('/trustap/payment/callback', [PaymentController::class, 'paymentCallback'])->name('trustap.payment.callback');
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Middleware\JwtMiddleware;
 use App\Http\Middleware\TrustapUser;
 use App\Models\EntityTrustapTransaction;
 use App\Services\v1\Payment\TrustAppException;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
@@ -29,27 +28,6 @@ Route::get('/auth/callback', function () {
     // $user->token
 });
 
-Route::get('all-log-files',function(){
-    $files = File::files(storage_path('logs'));
-    echo "<ul>";
-    foreach ($files as $file) {
-        echo "<li>".$file->getFilename()."</li>"; // Show log file name like laravel-2025-07-24.log
-    }
-    echo "</ul>";
-});
-
-Route::get('view-log-files/{date}', function($date = null){
-    if (empty($date)) {
-        return 'date is required';
-    }
-    $date = $date ?? now()->format('Y-m-d'); // Default to today
-    $logPath = storage_path("logs/laravel-{$date}.log");
-    if (!File::exists($logPath)) {
-        abort(404, 'Log file not found');
-    }
-    $logs = File::get($logPath); // Get the whole log content
-    return response()->view('logs-view', ['logs' => $logs]);
-});
 // Route::get('/payment', [PaymentController::class, 'store']);
 // Route::get('/payment', [PaymentController::class, 'store']);
 // Route::get('/trustap/payment/callback', [PaymentController::class, 'paymentCallback'])->name('trustap.payment.callback');
