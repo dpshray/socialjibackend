97263d3e139649c35b6f981dd6bae8296592ff66
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-08-07 17:07:56 +0545

    additional data added(admin successfull payment list count)

--- app/Http/Controllers/Api/v1/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Api/v1/Admin/AdminDashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\v1\Admin;
 
 use App\Constants\Constants;
+use App\Enums\PaymentStatusEnum;
 use App\Http\Controllers\Controller;
+use App\Models\EntityTrustapTransaction;
 use App\Models\Gig;
 use App\Models\User;
 use App\Traits\ResponseTrait;
@@ -20,6 +22,7 @@ class AdminDashboardController extends Controller
         $brand_count = User::role(Constants::ROLE_BRAND)->count();
         $influencer_count = User::role(Constants::ROLE_INFLUENCER)->count();
         $gig_count = Gig::count();
-        return $this->apiSuccess('admin dashboard', compact('brand_count','influencer_count','gig_count'));
+        $succesful_payment = EntityTrustapTransaction::paymentOverallComplete()->count(); 
+        return $this->apiSuccess('admin dashboard', compact('brand_count','influencer_count','gig_count','succesful_payment'));
     }
 }
