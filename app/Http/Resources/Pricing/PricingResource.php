02811bd5af93d307bf3a53b1aec3d03e42d8ac73
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-08-25 16:57:34 +0545

    delivery time date format changed

--- app/Http/Resources/Pricing/PricingResource.php
+++ app/Http/Resources/Pricing/PricingResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Pricing;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,31 +16,11 @@ class PricingResource extends JsonResource
     public function toArray(Request $request): array
     {
         // return parent::toArray($request);
-        /* 
-                    {
-                "id": 1,
-                "name": "basic",
-                "label": "Basic",
-                "created_at": null,
-                "updated_at": null,
-                "deleted_at": null,
-                "pivot": {
-                    "gig_id": 68,
-                    "pricing_tier_id": 1,
-                    "price": "5000.00",
-                    "delivery_time": "2025-06-22 21:00:00",
-                    "description": "tier updated description for price tier id 3",
-                    "requirement": "some updated  requirememnts for tier id",
-                    "currency_id": 1,
-                    "created_at": "2025-06-24T07:47:37.000000Z",
-                    "updated_at": "2025-06-24T07:47:56.000000Z"
-                }
-            }, */
         return [
             "id" => $this->id,
             "label" => $this->label,
             "price" => $this->pivot->price,
-            "delivery_time" => $this->pivot->delivery_time,
+            "delivery_time" => Carbon::parse($this->pivot->delivery_time)->format('d/m/Y'),
             "description" => $this->pivot->description,
             "requirement" => $this->pivot->requirement,
             "currency" => cache('currencies')->firstWhere('id',$this->pivot->currency_id)
