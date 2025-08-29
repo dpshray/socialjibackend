f3dbc083f0a8dea0a324902527b71bf95426427a
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-08-29 12:59:31 +0545

    data added for brand dashboard graph part

--- app/Http/Controllers/Api/v1/DashboardController.php
+++ app/Http/Controllers/Api/v1/DashboardController.php
@@ -10,6 +10,7 @@ use App\Http\Resources\Social\MinimalSocialProfileRequest;
 use App\Http\Resources\Social\MinimalSocialProfileResource;
 use App\Models\Gig;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -121,9 +122,36 @@ class DashboardController extends Controller
         
         $top_brand_with_max_followers = TopBrandInfluencerResource::collection($top_brand_with_max_followers);
 
+
+        $year = now()->format('Y');
+        $campaign_published_on_current_year =  DB::table('campaigns')
+            ->selectRaw('MONTH(created_at) as month, COUNT(*) as total')
+            ->whereYear('created_at', $year)
+            ->groupByRaw('MONTH(created_at)')
+            ->orderByRaw('MONTH(created_at)')
+            ->whereNull('deleted_at')
+            ->where('brand_id', $brand->id)
+            ->get();
+
+        $buyer_transactions = $brand->userTrustapMetadata->buyerTransactions;
+        $rawCounts = $buyer_transactions
+            ->filter(fn($t) => Carbon::parse($t->complaintPeriodDeadline)->year == $year) // only 2025
+            ->groupBy(fn($t) => Carbon::parse($t->complaintPeriodDeadline)->month) // group by month number (1–12)
+            ->map->count(); // [8 => 2, 9 => 3]
+
+        $currentMonth = Carbon::now()->month;
+        $gig_brought_on_current_year = collect(range(1, $currentMonth))->map(function ($month) use ($rawCounts) {
+            return [
+                'Month' => $month,
+                'gig_sold' => $rawCounts[$month] ?? 0
+            ];
+        })->values()->toArray();
+        
         $data = [
             'social_followers' => $social_followers,
             'top_brand_with_max_followers' => $top_brand_with_max_followers,
+            'own_campaign_published_on_current_year' => $campaign_published_on_current_year,
+            'gig_brought_on_current_year' => $gig_brought_on_current_year,
             'total_campaigns_added_count' => $total_campaigns_added_count,
             'total_reviews_given_count' => $total_reviews_given,
             'total_bidders_on_campaign_count' => $total_bidders_from_all_campaign_count,
